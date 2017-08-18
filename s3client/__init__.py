c524d85a490c55afb3132834c8acb80f27551c19
Junya Kaneko <jyuneko@gmail.com>
2017-08-18 09:41:58 +0900

    removed S3Client class.

--- s3client/__init__.py
+++ s3client/__init__.py
@@ -10,30 +10,20 @@ __author__ = 'Junya Kaneko <jyuneko@hotmail.com>'
 
 _cwd = []
 
+_session = None
+_s3 = None
+_bucket = None
 
-s3conf = toml.load(os.path.join(str(Path.home()), '.s3client'))
-
-
-class S3Client:    
-    def __init__(self, **kwargs):
-        session = boto3.session.Session(**kwargs)
-        self._s3 = session.resource('s3')
-
-    def __getattr__(self, name):
-        try:
-            return getattr(self._s3, name)
-        except AttributeError:
-            raise AttributeError
-            
-
-_s3client = S3Client()
+_conf = toml.load(os.path.join(str(Path.home()), '.s3client'))
 
 
-def set_new_client(**kwargs):
-    global _s3client
-    _s3client = S3Client(**kwargs)
-
+def init(**kwargs):
+    global _session, _s3, _bucket
+    _session = boto3.session.Session(**kwargs)
+    _s3 = session.resource('s3')
+    _bucket = _s3.Bucket(_conf['bucket'])
     
+
 def getcwd():
     return os.path.join('/', '/'.join(_cwd))
 
@@ -45,5 +35,6 @@ def chdir(path):
 
     
 def chbucket(bucket_name):
-    global s3conf
-    s3conf['bucket'] = bucket_name
+    global _conf, _bucket
+    _conf['bucket'] = bucket_name
+    _bucket = _s3.Bucket(_conf['bucket'])
